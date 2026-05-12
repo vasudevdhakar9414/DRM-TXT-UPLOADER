e03c0e4eaffda2989b33e4d3bdac254e9b88d775
vasudevdhakar9414 <nxsstranger79@gmail.com>
2026-05-12 21:33:57 +0530

    Change API_ID, API_HASH, and BOT_TOKEN values
    Updated API credentials in config.py.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = os.environ.get("API_ID", "24473318") #Replace With your api id
-API_HASH = os.environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5") #Replace With your api hash
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7566384654:AAHYfwA1rhqTbC9rzJ05Ki5N57_c1hNByZM") #Replace With your bot token
+API_ID = os.environ.get("API_ID", "22998429") #Replace With your api id
+API_HASH = os.environ.get("API_HASH", "ddf9f9ac47ce6d25da107bebc98c9234") #Replace With your api hash
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8223412974:AAFKZ2zGFE2UQBSHNCjITW3CIXi3bUQt_dI") #Replace With your bot token
